d1db1d22df7509c52653498f52626dc8d43b9d5d
Eric Stern <eric@ericstern.com>
2021-10-22 16:45:44 -0700

    give users ids

--- src/User.php
+++ src/User.php
@@ -13,10 +13,16 @@ use Firehed\U2F\{
 
 class User implements JsonSerializable
 {
+    private string $id;
     private string $name;
     private string $hash = '';
 
     private array $registrations = [];
+
+    public function __construct()
+    {
+        $this->id = self::uuidv4();
+    }
 
     public function setName(string $name)
     {
@@ -43,6 +49,7 @@ class User implements JsonSerializable
     public function jsonSerialize(): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'hash' => $this->hash,
             'registrations' => $this->registrations,
@@ -84,9 +91,27 @@ class User implements JsonSerializable
     public static function fromJson(array $data): User
     {
         $user = new User();
+        $user->id = $data['id'];
         $user->name = $data['name'];
         $user->hash = $data['hash'];
         $user->registrations = $data['registrations'];
         return $user;
     }
+
+    private static function uuidv4(): string
+    {
+        $bytes = random_bytes(16);
+        $bytes[6] = chr((ord($bytes[6])) & 0x0F | 0x40);
+        $hex = bin2hex($bytes);
+        return sprintf(
+            '%s-%s-%s-%s-%s',
+            substr($hex, 0, 8),
+            substr($hex, 8, 4),
+            substr($hex, 12, 4),
+            substr($hex, 16, 4),
+            substr($hex, 20, 12),
+        );
+    }
+
+
 }
