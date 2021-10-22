321a14078ad1e5d36be862e48abb3d93aa45ba86
Eric Stern <eric@ericstern.com>
2021-10-22 16:41:34 -0700

    Drop index and register endpoints in favor of old version

---
+++ public/register_post.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/bootstrap.php';
+
+assert(isset($_POST['username']));
+assert(isset($_POST['password']));
+
+$storage = new Firehed\Webauthn\UserStorage();
+
+$user = $storage->get($_POST['username']);
+if ($user) {
+    header('HTTP/1.1 409 Conflict');
+    echo "Already registered. Go back and use your user";
+    return;
+}
+
+$user = new Firehed\Webauthn\User();
+$user->setName($_POST['username']);
+$user->setPassword($_POST['password']);
+
+$storage->save($user);
+
+header('HTTP/1.1 200 OK');
+echo 'Registration succeeded. Go back to add a token.';
