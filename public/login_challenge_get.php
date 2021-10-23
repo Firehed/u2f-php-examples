c5d4b63206111a308bb9f014f378f7637d39746a
Eric Stern <eric@ericstern.com>
2021-10-22 17:20:18 -0700

    login with token pt1

---
+++ public/login_challenge_get.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+$app = require dirname(__DIR__) . '/bootstrap.php';
+$server = $app['server'];
+
+$storage = new Firehed\Webauthn\UserStorage();
+$user = $storage->get($_SESSION['USER_NAME']);
+assert($user !== null);
+
+$registrations = $user->getRegistrations();
+
+$signRequests = $server->generateSignRequests($registrations);
+
+$_SESSION['SIGN_REQUESTS'] = $signRequests;
+
+// WebAuthn expects a single challenge for all key handles, and the Server generates the requests accordingly.
+$data = [
+    'challenge' => $signRequests[0]->getChallenge(),
+    'keyHandles' => array_map(function (Firehed\U2F\SignRequest $sr) {
+        return $sr->getKeyHandleWeb();
+    }, $signRequests),
+];
+
+header('HTTP/1.1 200 OK');
+header('Content-type: application/json');
+echo json_encode($data);
